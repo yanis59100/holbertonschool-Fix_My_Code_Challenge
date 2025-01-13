5bc6401106f21747f62c4d2a5fc35d77b7d4dd62
yanis59100 <meftouh.yanis@hotmail.com>
2025-01-13 12:58:09 +0100

    Modifié parseInt(process.argv[2], 16) par parseInt(process.argv[2], 10)

---
+++ challenge/1-print_square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1);
+}
+
+const size = parseInt(process.argv[2], 10);
+
+if (isNaN(size) || size <= 0) {
+    process.stderr.write("Invalid size\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    console.log("#".repeat(size));
+}
